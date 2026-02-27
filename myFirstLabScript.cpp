d679f53ccb86ac436cf567da50c11b2b1d2a1bea
utku10a <62486277+utku10a@users.noreply.github.com>
2026-02-27 17:03:02 +0300

    Create myFirstLabScript.cpp

---
+++ myFirstLabScript.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+string isim;
+    cout << "Enter your name: ";
+    cin >> isim;
+    cout << "Your name is: " << isim << endl;
+int id;
+    cout << "Enter your id: ";
+    cin >> id;
+    cout << "Your id is: " << id << endl;
+
+
+    return 0;
+}
